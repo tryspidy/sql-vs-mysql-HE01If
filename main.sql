0facbd78e8440f03e656df26c878b9afc29c59cc
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 12:25:47 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,7 @@
+• SQL is a standard language for 
+retrieving and manipulating structured
+databases. On the contrary, 
+MySQL is a relational database management
+system, like SQL Server, Oracle or
+IBM DB2, that is used to manage SQL
+databases. 
